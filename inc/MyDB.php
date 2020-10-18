df50f8c1d679e2bf35be58b77ddfbc65a0822856
Bernd Engels <engels@goldenacker.de>
2020-10-18 13:02:29 +0200

    stuff

--- inc/MyDB.php
+++ inc/MyDB.php
@@ -20,7 +20,9 @@ class MyDB extends PDO {
         // DSN: data source name
         $dsn  = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
         $options    = array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            PDO::MYSQL_ATTR_INIT_COMMAND    => 'SET NAMES utf8',
+            PDO::ATTR_DEFAULT_FETCH_MODE    => $this->_outputFormat,
+            PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
         );
         parent::__construct($dsn, DB_USERNAME, DB_PASSWORD, $options);
     }
